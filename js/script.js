a432869e2b237cc0d3d234c2753efb58abc2a36c
JienHuaGoo <mypstom@gmail.com>
2016-02-07 12:39:20 +0800

    attack def view

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 $(function(){
 	$('.login-botton').click(function(){
-		$('.login').toggle();
-		$('.game').toggle();
+		$('.login').fadeToggle();
+		//$('.game').fadeToggle();
 	});	
+	$('#quit').click(function(){
+		$('.login').fadeIn();
 
+	});
+	$('#test1').click(function(){
+		$('.attack-view ').fadeToggle();
+	});
+	$('#test2').click(function(){
+		$('.def-view ').fadeToggle();
+	});
 
 
 
